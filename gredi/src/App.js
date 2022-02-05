d3954ac64df1a1f9ffa5201ee007ce3566e86c90
Pranav Mangal <pranav.mangal18@imperial.ac.uk>
2022-02-05 22:48:20 +0000

    Fix extension URL and parse URL in frontend

--- gredi/src/App.js
+++ gredi/src/App.js
@@ -1,15 +1,30 @@
 import * as React from "react";
-import { BrowserRouter } from "react-router-dom";
+
+import { useEffect, useState } from "react";
 
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 
 export default function App() {
+  const [url, setUrl] = useState("");
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    setUrl(params.get("url"));
+  }, []);
+
+  useEffect(() => {
+    fetch(`https://autoshop-ic.vercel.app/api?url=${url}`)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, [url]);
+
   return (
     <Container>
       <Box>
-        <Typography>Create React App</Typography>
+        <Typography>{url}</Typography>
       </Box>
     </Container>
   );
